b1c7cb541e3fa30c0287a1f13d10170498d2e8fe
zhangwei <zhangwei@zhangwei-ThinkPad-X230>
2018-07-31 23:03:41 +0800

    zhang

--- src/main/java/com/tietie/service/impl/AtUserServiceImpl.java
+++ src/main/java/com/tietie/service/impl/AtUserServiceImpl.java
@@ -22,75 +22,21 @@ public class AtUserServiceImpl implements AtUserService {
 	private AtUserMapper userDao;
 
 	@Override
-	@Transactional
 	public CommonReturnBean userRegist(AtUser userModel) {
-		CommonReturnBean result = new CommonReturnBean();
-
-		// action校验参数
-
-		// Set<ConstraintViolation<AtUser>> constraintViolations =
-		// ValidationUtil.getValidation().validate(userModel);
-		// if (constraintViolations.size() == 0) {
-		// } else {
-		// result.setCode(SystemCodeEnum.PARAMETER_ERROR.getc);
-		// for (ConstraintViolation<AtUser> constraintViolation :
-		// constraintViolations) {
-		// System.out.println(constraintViolation.getMessage());
-		// result.setMessage(constraintViolation.getMessage());
-		// }
-		// }
-		try {
-
-			// 加密操作
-			userModel
-					.setPwd(encodePwd(userModel.getPwd(), userModel.getCode()));
-			userDao.insert(userModel);
-			result.setCode(SystemCodeEnum.SUCCESS.getCode());
-		} catch (Exception e) {
-			result.setCode(SystemCodeEnum.DATABASE_ERROR.getCode());
-			e.printStackTrace();
-		}
-
-		return result;
-	}
-
-	private String encodePwd(String pwd, String code) {
-		return EncryptionUtil.encryptionUserPwd(pwd, code);
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
 	public CommonReturnBean userLogin(String userCode, String pwd) {
-		CommonReturnBean result = new CommonReturnBean();
-		String newPwd = encodePwd(pwd, userCode);
-		Map<String, String> param = new HashMap<String, String>();
-		param.put("code", userCode);
-		param.put("pwd", newPwd);
-		Map<String,Integer> tempResult = userDao.selectByCodePwd(param);
-		if (tempResult.get("id")!=null) {
-			result.setCode(SystemCodeEnum.SUCCESS.getCode());
-			
-			Map<String,Object> tempMap = new HashMap<String, Object>();
-			tempMap.put("id", tempResult.get("id"));
-			result.setCommonEntity(tempMap);
-		} else {
-			result.setCode(SystemCodeEnum.PARAMETER_ERROR.getCode());
-		}
-
-		return result;
+		return null;
 	}
 
 	@Override
 	public CommonReturnBean userCancelled(String userCode, String pwd) {
-		CommonReturnBean result = new CommonReturnBean();
-		Map<String, String> param = new HashMap<String, String>();
-		param.put("code", userCode);
-		param.put("pwd", pwd);
-		int tempResult = userDao.updateByCodePwd(param);
-		if (tempResult == 1) {
-			result.setCode(SystemCodeEnum.SUCCESS.getCode());
-		} else {
-			result.setCode(SystemCodeEnum.PARAMETER_ERROR.getCode());
-		}
+		// TODO Auto-generated method stub
 		return null;
 	}
+
+	
 }
