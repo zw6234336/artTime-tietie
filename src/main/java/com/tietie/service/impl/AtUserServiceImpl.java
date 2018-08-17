49077bf125dcfe892d23d8ef04c3eb0ef681b9c5
76024 <76024@LAPTOP-JPSKA5AV>
2018-08-17 14:36:36 +0800

    zhang

--- src/main/java/com/tietie/service/impl/AtUserServiceImpl.java
+++ src/main/java/com/tietie/service/impl/AtUserServiceImpl.java
@@ -1,19 +1,13 @@
 package com.tietie.service.impl;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.tietie.bean.CommonReturnBean;
 import com.tietie.dao.AtUserMapper;
-import com.tietie.enumm.SystemCodeEnum;
 import com.tietie.model.AtUser;
 import com.tietie.service.AtUserService;
-import com.tietie.util.EncryptionUtil;
 
 @Service
 public class AtUserServiceImpl implements AtUserService {
